266d106236114b84f9441a5601547fcf5d20ae24
digidriver <slippers99@hotmail.com>
2017-04-14 00:06:27 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    name: string;
+    blurb: string;
+    blurbTm: string;
+    blurb2: string;
+    product: string;
+    productTm: string;
+    teaserDate: string;
+
+    constructor() {
+        this.name = 'Angular 2';
+        this.blurb = 'A brand new addition to the On The Go';
+        this.blurbTm = 'TM';
+        this.blurb2 = ' suite of available products is coming soon:';
+        this.product = 'Profile Control';
+        this.productTm = 'TM';
+        this.teaserDate = " - Coming Fall 2017";
+    }
 }
